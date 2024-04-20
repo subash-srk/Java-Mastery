8d54e95a3f00f4eb51fa010cf597a7ca286bc44a
Subash K <81848034+subash-srk@users.noreply.github.com>
2024-04-20 20:32:52 +0530

    Add files via upload

---
+++ Core Java/arraydemo.java
@@ -0,0 +1,15 @@
+public class arraydemo {
+    public static void main(String[] args) {
+        int nums[] = new int[4];
+        int numericals[] = {1, 2, 4, 5};
+        nums[0] = 1;
+        nums[1] = 2;
+        nums[2] = 5;
+        nums[3] = 7;
+        for(var i=0; i<4; i++){
+            System.out.println(numericals[i]);
+        }
+        
+    }
+    
+}
