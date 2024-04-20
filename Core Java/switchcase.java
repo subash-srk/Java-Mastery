c2ee7fd498ff888c44e2e679fb0831bb47c993fd
Subash K <81848034+subash-srk@users.noreply.github.com>
2024-04-20 20:36:09 +0530

    Add files via upload

---
+++ Core Java/switchcase.java
@@ -0,0 +1,16 @@
+public class switchcase {
+    public static void main(String[] args){
+        int n = 2;
+        switch(n){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+        }
+    }
+}
