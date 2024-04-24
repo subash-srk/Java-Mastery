0e9038beaef1264a90032ea6e18909f30edb95f2
Subash K <81848034+subash-srk@users.noreply.github.com>
2024-04-24 19:48:51 +0530

    Add files via upload

---
+++ Core Java/StringsDemo.java
@@ -0,0 +1,14 @@
+public class StringsDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        String name = new String("Subash");
+        System.out.println(name);
+        String s1 = "XOTWOD";
+        System.out.println(s1);
+
+        StringBuffer clan = new StringBuffer("XO");
+        System.out.println(clan);
+    }
+    
+}
